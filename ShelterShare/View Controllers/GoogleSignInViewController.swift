f360e7e18e5fea814bd0a6bdb06a9b7bb26c76a5
AlexanderHallCS <hallale21@yahoo.com>
2022-01-30 02:57:26 -0600

    Remove Location VC

--- ShelterShare/View Controllers/GoogleSignInViewController.swift
+++ ShelterShare/View Controllers/GoogleSignInViewController.swift
@@ -56,7 +56,7 @@ class GoogleSignInViewController: UIViewController {
         }
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -64,6 +64,5 @@ class GoogleSignInViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
